7d631a3e50dcd5523978f14378fd7ea4b2f51479
xiongzf <xiongzf>
2022-06-14 16:14:12 +0800

    新增

---
+++ provider/src/main/java/cn/xzf/provider/ProviderApplication.java
@@ -0,0 +1,16 @@
+package cn.xzf.provider;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@MapperScan("mapper")
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+}
